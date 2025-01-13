a921bac15cb84ec59304a6c4cca03142aa527f01
MorganASJ <morganjones2000@gmail.com>
2025-01-13 21:18:05 +0000

    updated error checking

--- monocheck.py
+++ monocheck.py
@@ -11,14 +11,12 @@ Entrez.email = "Morgan.Jones@bristol.ac.uk"
 def animated_loading(message):
     chars = "/—\|" 
     for char in chars:
-        sys.stdout.write('\r' + char + message +'\r')
+        sys.stdout.write('\r' + char + message)
         time.sleep(.1)
         sys.stdout.flush()
 
 def request_taxonomy_data(taxa_list):
     taxonomy_data = []
-
-    start_time = time.time()
 
     # For each taxon make a request to Entrez NCBI Taxonomy
     for taxon in taxa_list:
@@ -28,8 +26,9 @@ def request_taxonomy_data(taxa_list):
             search_result = Entrez.read(search)
             search.close()
 
+            # If no results returned
             if not search_result["IdList"]:
-                print(f"Erorr: Taxon not found: {taxon}")
+                print(f"\rWARNING: Unable to find records on NCBI Taxonomy for: {taxon}. \nPlease check tip labels or manually insert information into taxonomy file.")
                 continue
 
             # Fetch taxonomy details
@@ -48,89 +47,26 @@ def request_taxonomy_data(taxa_list):
         except Exception as e:
             print(f"Error fetching data for {taxon}: {e}")
 
-    print("--- %s seconds ---" % (time.time() - start_time))
-
-    return taxonomy_data
-
-def request_taxonomy_data1(taxa_list):
-    taxonomy_data = []
-
-    start_time = time.time()
-
-    sterm = " OR ".join(taxa_list)
-
-    # Search for the taxon in NCBI Taxonomy
-    search = Entrez.esearch(db="taxonomy", term=sterm)
-    search_result = Entrez.read(search)
-    search.close()
-
-    if not search_result["IdList"]:
-        print(f"Erorr: Taxon not found: {sterm}")
-        raise Exception("Id List Empty")
-
-    # For each taxon make a request to Entrez NCBI Taxonomy
-
-    # Fetch taxonomy details
-
-    for tax_id in search_result["IdList"]:
-        fetch = Entrez.efetch(db="taxonomy", id=tax_id, retmode="xml")
-        fetch_result = Entrez.read(fetch)
-        fetch.close()
-
-        #Retreive taxonomy details
-        tax_data = fetch_result[0]
-        lineage_dict = {rank["Rank"]: rank["ScientificName"] for rank in tax_data["LineageEx"]}
-        lineage_dict["Species"] = tax_data["ScientificName"]
-        lineage_dict["Taxon"] = "_".join(lineage_dict["Species"].split())
-        taxonomy_data.append(lineage_dict)
-
-    for f in taxonomy_data:
-        print(f['Taxon'])
-
-    print("--- %s seconds ---" % (time.time() - start_time))
-
     return taxonomy_data
 
 def fetch_taxonomy(taxa_list, taxonomy_file, wrapper):
-    taxonomy_data = {}
-    # parts = len(taxa[0].split("_"))
-
+    # Request taxonomy records from NCBI
+    records = request_taxonomy_data(taxa_list)
     
-    # print(taxa_list)
+    # Create a DataFrame and save to CSV
+    taxonomy_data = pd.DataFrame(records).fillna("N/A")
+    taxonomy_data.to_csv(taxonomy_file, index=False)
 
-    # First check if a file exists
-    if os.path.exists(taxonomy_file):
-        print(f"Using existing taxonomy file: {taxonomy_file}")
-        try:
-            # records = pd.read_csv(taxonomy_file).to_dict(orient='records')
-            taxonomy_data = pd.read_csv(taxonomy_file)   
-   
-        except:
-            raise Exception("Failure to read Taxonomy File")
-    
-    else:
-
-        # return 0
-        records = request_taxonomy_data(taxa_list)
-        # Create a DataFrame and save to CSV
-        taxonomy_data = pd.DataFrame(records).fillna("N/A")
-        taxonomy_data.to_csv(taxonomy_file, index=False)
-        print(f"Taxonomy data saved to {taxonomy_file}")
-
-        
-    taxonomy_data.columns = map(str.lower, taxonomy_data.columns)
-    # for rec in records:
-        # taxonomy_data[rec['Taxon']] = rec
+    # Place taxonomy in mutable object so that it can be accessed by the main thread upon completion
     wrapper.append(taxonomy_data)
-    return taxonomy_data
+    return 1
 
 def read_taxonomy(filepath):
-    print(f"Using existing taxonomy file: {filepath}")
+    print(f"Reading in existing taxonomy file: {filepath}")
     try:
         taxonomy_data = pd.read_csv(filepath)   
-
     except:
-        raise Exception("Failure to read Taxonomy File")
+        raise Exception("Taxonomy file could not be read")
     
     return taxonomy_data
 
@@ -181,6 +117,7 @@ def main():
     parser.add_argument("-outgroup", required=True, help="File containing outgroup taxa (one per line)")
     parser.add_argument("-taxonomy", required=False, help="File containing taxonomy information")
     parser.add_argument("--showtree", action=argparse.BooleanOptionalAction, required=False, help="Shows the rooted tree prior to checking for Monophyly of clade")
+    parser.add_argument("--resettaxonomy", action=argparse.BooleanOptionalAction, required=False, help="Resets the taxonomy file by redownloading information from NCBI Taxonomy")
     args = parser.parse_args()
 
     taxonomy=[]
@@ -190,111 +127,107 @@ def main():
     # create tree object from tree
     try:
         if not os.path.exists(args.tree):
-            raise Exception("Tree file does not exist at path:", args.tree)
+            raise Exception(f"Tree file does not exist at path: {args.tree}")
         tree = Tree(args.tree, format=1)
 
         leaf_names = [leaf.name for leaf in tree.get_leaves()]
         taxa_list = ["_".join((taxon[0],taxon[1])) for taxon in [taxon.split("_") for taxon in leaf_names]]
+    except IndexError as err:
+        print("ERROR PROCESSING TIP LABEL: Ensure tip labels are in format Genus_species*")
+        sys.exit(1)
     except Exception as err:
-        print("ERROR PROCESSING TREE: ", err)
+        print("ERROR PROCESSING TREE: ", err.args[0])
         sys.exit(1)
 
     # Read in outgroup file
     try:
         # Root the tree
         if not os.path.exists(args.outgroup):
-            raise Exception("Outgroup file does not exist at path:", args.outgroup)
+            raise Exception(f"Outgroup file does not exist at path: {args.outgroup}")
         tree = root_tree(tree, args.outgroup)
 
         if args.showtree:
             print(tree) # Works
 
     except Exception as err:
-        print("ERROR PROCESSING OUTGROUP FILE: ", err)
+        print("ERROR PROCESSING OUTGROUP FILE: ", err.args[0])
         sys.exit(1)
     
     if args.taxonomy:
 
         try:
             if not os.path.exists(args.taxonomy):
-                raise Exception("taxonomy file does not exist at path:", args.taxonomy)
+                raise Exception(f"Taxonomy file does not exist at path: {args.taxonomy}")
             
             taxonomy = read_taxonomy(args.taxonomy)
 
-            if taxa_list not in taxonomy['Taxon']:
-                raise Exception("Taxonomy file does not contain all the taxa in tree file\nMissing taxa:", taxa_list - taxonomy['Taxon'])
-
         except Exception as err:
-            print("ERROR READING TAXONOMY FILE:", err)
+            print("ERROR READING TAXONOMY FILE:", err.args[0])
             sys.exit(1)
 
     else:
-
-        try:
-            found = False
-            for file in os.listdir():
-                if file.endswith(".tax"):
-                    if found:
-                        raise Exception("Multiple taxonomy files present")
-                    # print(file)
-                    taxonomy = read_taxonomy(file)
-                    # print(taxonomy)
-                    found = True
-        except Exception as err:
-            print("ERROR SEARCHING FOR TAXONOMY FILE:", err)
-            sys.exit(1)
+        found = False
+        if not args.resettaxonomy:
+            try:
+                
+                for file in os.listdir():
+                    if file.endswith(".tax"):
+                        if found:
+                            raise Exception("Multiple taxonomy files present")
+                        # print(file)
+                        taxonomy = read_taxonomy(file)
+                        # print(taxonomy)
+                        found = True
+            except Exception as err:
+                print("ERROR SEARCHING FOR TAXONOMY FILE:", err.args[0])
+                sys.exit(1)
 
         try:
 
             if not found:
                 # Calculate new taxonomy
                 wrapper = []
+                start_time = time.time()
                 ret_taxonomy = threading.Thread(name='retrieve_taxonomy', target=fetch_taxonomy, args=(taxa_list, "taxonomy.tax", wrapper))
                 ret_taxonomy.start()
                 while ret_taxonomy.is_alive():
-                    animated_loading('Retriving taxonomy information for', len(taxa_list),'taxa...')
+                    animated_loading(f' Retriving taxonomy information for {len(taxa_list)} taxa...')
+                
+                sys.stdout.flush()
+                print(f'\rRetrieving taxonomy information from NCBI Taxonomy database took {round(time.time() - start_time,1)} seconds.')
+
                 if not wrapper:
                     raise Exception("No taxonomy returned by retrieval thread")
                     
-                
                 taxonomy = wrapper[0]
 
         except Exception as err:
-            print("ERROR GENERATING TAXONOMY:", err)
+            print("ERROR GENERATING TAXONOMY:", err.args[0])
             sys.exit(1)
-            
-    # # Load or generate taxonomy
-    # taxonomy_file = "taxonomy.tax"
-    
-    # taxa_list = ["_".join((taxon[0],taxon[1])) for taxon in [taxon.split("_") for taxon in leaf_names]]
 
-    # # taxonomy = fetch_taxonomy(taxa_list, taxonomy_file)
+    # Check no taxa are missing from taxonomy before proceeding
+    try:
+        missing_taxa = set(taxa_list) - set(taxonomy['Taxon'])
+        if missing_taxa:
+            raise Exception(f"The following taxa are missing: {missing_taxa}")
+    except Exception as err:
+        print("ERROR READING TAXONOMY: ", err.args[0])
+        sys.exit(1)
 
-    # wrapper = []
+    # Check monophyly
+    try:
+        result = is_monophyletic(tree, str.lower(args.clade), taxonomy, leaf_names)
 
-    # the_process = threading.Thread(name='process', target=fetch_taxonomy, args=(taxa_list, taxonomy_file, wrapper))
+        print(f"Is {args.clade} monophyletic? {'Yes' if result[0] else 'No'}")
 
-    # the_process.start()
+        if not result[0]:
+            print(f'Monophyly is prevented by the inclusion of the following leaves:')
+            for leaf in result[2]:
+                print(leaf.name)
 
-    # time.sleep(0.1)
-
-    # while the_process.is_alive():
-    #     animated_loading()
-
-    # if not wrapper:
-    #     raise Exception("Failure to retrieve Taxonomy")
-    # else:
-    #     taxonomy = wrapper[0]
-
-        # Check monophyly
-    result = is_monophyletic(tree, str.lower(args.clade), taxonomy, leaf_names)
-    # return 1
-    print(f"Is {args.clade} monophyletic? {'Yes' if result[0] else 'No'}")
-
-    if not result[0]:
-        print(f'Monophyly is prevented by the inclusion of the following leaves:')
-        for leaf in result[2]:
-            print(leaf)
+    except Exception as err:
+        print("ERROR CALCULATING MONOPHYLY:", err.args[0])
+        sys.exit(1)    
 
 if __name__ == "__main__":
     main()
